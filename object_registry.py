6486f4e39e64b8cd97dd34de9b6712755d365b45
Alex Davies <traverse.da@gmail.com>
2016-04-08 14:47:44 -0300

    Removed extra prints from object_registry, which does seem to be working.

--- object_registry.py
+++ object_registry.py
@@ -1,10 +1,8 @@
 block_types={}
 
 def register_block(name):
-    print("name: "+name)
     def wrapper(block):
         global block_types
-        print(block)
         if hasattr(block_types, name):
             print("WARNING: block of type {name} already exists. Replacing {orig} with {new}".format(
                     name=name, orig=block_types[name], new=func
