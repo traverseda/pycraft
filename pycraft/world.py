934a9f7e5e35e53c5eb82d3e6b8fd8c3b644b958
TristanTrim <tristantrim@gmail.com>
2016-04-21 11:30:36 -0300

    Star imports irk me. I was in a faye mood.

--- pycraft/world.py
+++ pycraft/world.py
@@ -3,7 +3,13 @@ from collections import deque
 
 from noise.perlin import SimplexNoise
 from pyglet import image
-from pyglet.gl import *
+from pyglet.gl import glClearColor, glEnable, GL_CULL_FACE, glTexParameteri, \
+                         GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST, \
+                         GL_TEXTURE_MAG_FILTER, GL_FOG, glFogfv, GL_FOG_COLOR,\
+                         GLfloat, glHint, GL_FOG_HINT, GL_DONT_CARE, glFogi, \
+                         GL_FOG_MODE, GL_LINEAR, glFogf, GL_FOG_START, \
+                         GL_FOG_END, GL_QUADS
+
 from pyglet.graphics import Batch, TextureGroup
 from pyglet.window import mouse
 
