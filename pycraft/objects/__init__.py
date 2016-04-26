7ee60b6f42fee3bbd9b7e49c1c372c0568f442fe
Hispar <hispar@gmail.com>
2016-04-26 20:31:01 +0200

    Remove not completed code

--- pycraft/objects/__init__.py
+++ pycraft/objects/__init__.py
@@ -1,9 +1,7 @@
 from .block import Brick, Grass, Sand, Stone, WeakStone
-from .blocks.water import Water
 
 brick = Brick()
 grass = Grass()
 sand = Sand()
 weak_stone = WeakStone()
 stone = Stone()
-water = Water()
